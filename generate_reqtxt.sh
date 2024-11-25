2981e1cc56b6f2ce188a498ed6c44341e438191e
motey <bleimehl@gmail.com>
2024-11-25 10:56:59 +0100

    wip

--- generate_reqtxt.sh
+++ generate_reqtxt.sh
@@ -2,4 +2,4 @@
 rm backend/requirements.txt
 python -m pip install pip-tools -U
 python -m piptools compile -o ./backend/requirements.txt ./backend/pyproject.toml
-python -m pip install -r ./backend/requirements.txt -U
+#python -m pip install -r ./backend/requirements.txt -U
